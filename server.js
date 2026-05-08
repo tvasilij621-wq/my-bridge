b10c1ee1fc06ffca3989e6c732cca1a4e24bf29f
tvasilij621-wq <tvasilij621@gmail.com>
2026-05-09 00:45:44 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,22 +5,28 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } }); // Для стабильности на Render
 
 app.use(express.static(__dirname));
 
 let players = {};
 
 io.on('connection', (socket) => {
-    // Создаем игрока со случайным цветом
-    players[socket.id] = { 
-        x: 0, y: 1.6, z: 5, 
-        color: '#' + Math.floor(Math.random()*16777215).toString(16) 
-    };
+    console.log('Попытка подключения:', socket.id);
 
-    socket.emit('currentPlayers', players);
-    socket.broadcast.emit('newPlayer', { id: socket.id, info: players[socket.id] });
+    // Логика входа с ником
+    socket.on('join-simulation', (data) => {
+        console.log('Игрок вошел в симуляцию:', data.nick);
+        players[socket.id] = { 
+            x: 0, y: 1.6, z: 20, 
+            nick: data.nick,
+            color: '#' + Math.floor(Math.random()*16777215).toString(16) 
+        };
+        socket.emit('currentPlayers', players);
+        socket.broadcast.emit('newPlayer', { id: socket.id, info: players[socket.id] });
+    });
 
+    // Движение
     socket.on('playerMovement', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
@@ -30,11 +36,22 @@ io.on('connection', (socket) => {
         }
     });
 
+    // ЧАТ
+    socket.on('chatMessage', (msg) => {
+        if(players[socket.id]) {
+            console.log(`[CHAT] ${players[socket.id].nick}: ${msg}`);
+            io.emit('chatMessage', { id: socket.id, msg: msg });
+        }
+    });
+
     socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('playerDisconnected', socket.id);
+        console.log('Игрок ушел:', socket.id);
+        if(players[socket.id]) {
+            delete players[socket.id];
+            io.emit('playerDisconnected', socket.id);
+        }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Сервер готов! Адрес: http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`SYSTEM: ONLINE | PORT: ${PORT}`));
